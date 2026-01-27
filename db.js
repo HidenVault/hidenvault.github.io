fd207570cea5bd3f0a5ea2245c2fabbf11bd5c58
TakkunMC_jp <Kaneda.family.803@gmail.com>
2026-01-27 16:05:46 +0900

    Update db.js

--- db.js
+++ db.js
@@ -1,12 +1,16 @@
 let db;
 const req = indexedDB.open("vault",1);
-req.onupgradeneeded = e=>{
-  db = e.target.result;
+req.onupgradeneeded=e=>{
+  db=e.target.result;
   db.createObjectStore("files",{keyPath:"name"});
 };
-req.onsuccess = e=> db = e.target.result;
+req.onsuccess=e=>db=e.target.result;
 
-function saveFile(obj){
-  const tx = db.transaction("files","readwrite");
+function put(obj){
+  const tx=db.transaction("files","readwrite");
   tx.objectStore("files").put(obj);
+}
+function getAll(cb){
+  const tx=db.transaction("files");
+  tx.objectStore("files").getAll().onsuccess=e=>cb(e.target.result||[]);
 }
